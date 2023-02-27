948fe1f4825231e2e991cd7b242ef805ca572fd0
smercas <smercas@gmail.com>
2023-02-27 16:53:21 +0200

    tema laborator 2

--- laborator2_2/laborator2_2/global_functions.cpp
+++ laborator2_2/laborator2_2/global_functions.cpp
@@ -2,27 +2,27 @@
 #include "Student.h"
 #include "global_functions.h"
 
-int cmpName(Student A, Student B) {
+int cmpName(Student &A, Student &B) {
 	if (strcmp(A.getName(), B.getName()) < 0) return -1;
 	else if (strcmp(A.getName(), B.getName()) > 0) return 1;
 	else return 0;
 }
-int cmpMathematics(Student A, Student B) {
+int cmpMathematics(Student &A, Student &B) {
 	if (A.getMathematicsGrade() < B.getMathematicsGrade()) return -1;
 	else if (A.getMathematicsGrade() > B.getMathematicsGrade()) return 1;
 	else return 0;
 }
-int cmpEnglish(Student A, Student B) {
+int cmpEnglish(Student &A, Student &B) {
 	if (A.getEnglishGrade() < B.getEnglishGrade()) return -1;
 	else if (A.getEnglishGrade() > B.getEnglishGrade()) return 1;
 	else return 0;
 }
-int cmpHistory(Student A, Student B) {
+int cmpHistory(Student &A, Student &B) {
 	if (A.getHistoryGrade() < B.getHistoryGrade()) return -1;
 	else if (A.getHistoryGrade() > B.getHistoryGrade()) return 1;
 	else return 0;
 }
-int cmpAverage(Student A, Student B) {
+int cmpAverage(Student &A, Student &B) {
 	if (A.getAverage() < B.getAverage()) return -1;
 	else if (A.getAverage() > B.getAverage()) return 1;
 	else return 0;
--- laborator2_2/laborator2_2/global_functions.h
+++ laborator2_2/laborator2_2/global_functions.h
@@ -1,6 +1,6 @@
 #pragma once
-int cmpName(Student A, Student B);
-int cmpMathematics(Student A, Student B);
-int cmpEnglish(Student A, Student B);
-int cmpHistory(Student A, Student B);
-int cmpAverage(Student A, Student B);
+int cmpName(Student &A, Student &B);
+int cmpMathematics(Student &A, Student &B);
+int cmpEnglish(Student &A, Student &B);
+int cmpHistory(Student &A, Student &B);
+int cmpAverage(Student &A, Student &B);
